0cc7442988e6bee0b61f41f53115abcc771fa2fb
drakodarpan81 <drakodarpan@gmail.com>
2022-03-11 14:46:18 -0700

    Se anexa destroy

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -14,10 +14,33 @@ class PhotosController < ApplicationController
 
         photo.save
 
-        redirect_to "photos/#{photo.id}"
+        # redirect_to "/photos/#{photo.id}"
+        redirect_to photo
     end
 
     def show
         @photo = Photo.find(params[:id])
+    end
+
+    def edit
+        @id = params[:id]
+    end
+
+    def update
+        photo = Photo.find(params[:id])
+
+        photo.title = params[:photo][:title]
+        photo.image_url = params[:photo][:image_url]
+        photo.save
+
+        redirect_to photo
+    end
+
+    def destroy
+        photo = Photo.find(params[:id])
+        photo.destroy
+        put("photo.errors")
+
+        redirect_to "/photos"
     end
 end
